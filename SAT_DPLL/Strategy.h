5493d01de9860a6794ad05477d2a3ddc71a239a0
Egor <120785170+SCHVRevan@users.noreply.github.com>
2025-04-30 13:51:13 +0700

    Update Strategy.h
    Отделение реализации от boolequation

--- SAT_DPLL/Strategy.h
+++ SAT_DPLL/Strategy.h
@@ -1,6 +1,6 @@
 #ifndef STRATEGY_H
 #define STRATEGY_H
-//#include "boolequation.h"
+#include "boolequation.h"
 
 // Общий интерфейс всех стратегий
 class IStrategy {
@@ -8,18 +8,101 @@ public:
     virtual int strat(BoolEquation &) = 0;
     virtual ~IStrategy() = default;
 };
+
 // Каждая конкретная стратегия реализует общий интерфейс своим способом
 class Strategy1: public IStrategy {
 public:
     int strat(BoolEquation &be) {
-        return be.ChooseColForBranching();
+        //return be.ChooseColForBranching();
+        vector<int> indexes;
+        vector<int> values;
+        bool rezInit = false;
+
+        for (int i = 0; i < be.get_mask().getSize(); i++) {
+            if (be.get_mask()[i] == 0) {
+                indexes.push_back(i);
+            }
+        }
+
+        for (int i = 0; i < be.get_cnfSize(); i++) {
+            BoolInterval *interval = be.get_cnf()[i];
+
+            if (interval != nullptr) {
+                if (!rezInit) {
+                    for (int k = 0; k < indexes.size(); k++) {
+                        if (interval->getValue(indexes.at(k)) == '-') {
+                            values.push_back(1);
+                        } else {
+                            values.push_back(0);
+                        }
+                    }
+
+                    rezInit = true;
+                } else {
+                    for (int k = 0; k < indexes.size(); k++) {
+                        if (interval->getValue(indexes.at(k)) == '-') {
+                            //int val = values.at(k) + (interval->getValue(indexes.at(k)) - '0');
+                            values.at(k)++;
+                        }
+                    }
+                }
+            }
+        }
+
+        int minElementIndex = std::min_element(values.begin(), values.end()) - values.begin();
+
+        return indexes.at(minElementIndex);
     }
 };
 
 class Strategy2: public IStrategy {
 public:
     int strat(BoolEquation &be) {
-        return be.ChooseRowForBranching();
+        //return be.ChooseRowForBranching();
+        vector<int> nonEmptyRows;
+        vector<int> rowWeights;
+
+        // Собираем непустые строки (интервалы)
+        for (int i = 0; i < be.get_cnfSize(); i++) {
+            if (be.get_cnf()[i] != nullptr) {
+                nonEmptyRows.push_back(i);
+
+                // Вычисляем вес строки (количество незамаскированных переменных)
+                int weight = 0;
+                for (int j = 0; j < be.get_mask().getSize(); j++) {
+                    if (be.get_mask()[j] == 0 && be.get_cnf()[i]->getValue(j) != '-') {
+                        weight++;
+                    }
+                }
+                rowWeights.push_back(weight);
+            }
+        }
+
+        // Если нет строк, возвращаем -1 (ошибка)
+        if (nonEmptyRows.empty()) {
+            return -1;
+        }
+
+        // Выбираем строку с минимальным весом (но не нулевым)
+        int minIndex = 0;
+        int minWeight = INT_MAX;
+
+        for (size_t i = 0; i < rowWeights.size(); i++) {
+            if (rowWeights[i] > 0 && rowWeights[i] < minWeight) {
+                minWeight = rowWeights[i];
+                minIndex = i;
+            }
+        }
+
+        // Для выбранной строки ищем индекс переменной (столбец) для ветвления
+        int rowIndex = nonEmptyRows[minIndex];
+
+        // Выбираем первый незамаскированный столбец в этой строке
+        for (int j = 0; j < be.get_mask().getSize(); j++) {
+            if (be.get_mask()[j] == 0 && be.get_cnf()[rowIndex]->getValue(j) != '-') {
+                return j;
+            }
+        }
     }
 };
 
